66dcc6f68ece0eb604647355a9bfa0f40e832ddd
turkandagdeviren <turkandagdeviren@yahoo.com>
2022-07-30 16:32:04 +0200

    day3

---
+++ src/test/java/com/cybertek/day3/SpartanTestsWithParameters.java
@@ -0,0 +1,38 @@
+package com.cybertek.day3;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SpartanTestsWithParameters {
+    @BeforeAll
+    public static void init(){
+        baseURI = "http://44.204.228.18:8000";    }
+
+
+
+      /*   Given accept type is Json
+          And Id parameter value is 5
+          When user sends GET request to /api/spartans/{id}
+          Then response status code should be 200
+          And response content-type: application/json
+          And "Blythe" should be in response payload
+       */
+
+    @DisplayName("GET request to /api/spartans/{id} with ID 5")
+    @Test
+    public void test1(){
+        Response response = given().accept(ContentType.JSON)
+                            .and().pathParam("id", 5)
+                            .and().get("/api/spartans/{id}");
+
+        assertEquals(200, response.statusCode());
+        assertEquals("application/json", response.contentType());
+        assertTrue(response.body().asString().contains("Blythe"));
+
+    }
+}
