1aad8250623125c49ab67a705e526292c6b0298e
turkandagdeviren <turkandagdeviren@yahoo.com>
2022-08-08 22:15:17 +0200

    day5 HamcrestMatcher Intro

---
+++ src/test/java/com/cybertek/day5/HamcrestMatchersApiTest.java
@@ -0,0 +1,32 @@
+package com.cybertek.day5;
+
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+public class HamcrestMatchersApiTest {
+       /*
+       given accept type is Json
+       And path param id is 15
+       When user sends a get request to spartans/{id}
+       Then status code is 200
+       And content type is Json
+       And json data has following
+           "id": 15,
+           "name": "Meta",
+           "gender": "Female",
+           "phone": 1938695106
+        */
+
+    @DisplayName("OneSpartan with Hamcrest and chaining")
+    @Test
+    public void test1(){
+
+
+
+    }
+}
